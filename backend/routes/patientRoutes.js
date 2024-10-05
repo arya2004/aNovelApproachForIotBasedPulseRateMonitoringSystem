85c6086aa4609fc12a2b4b2899bd0fa3daed767a
PiyushXCoder <piyush.raj.kit@gmail.com>
2024-10-05 16:07:11 +0530

    fix: Missing requirements
    These missing requirements were causing server to fail while running

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
 const { isLoggedin } = require('../middleware/authMiddleware');
+const connectLiveReload = require("connect-livereload");
 
 router.get('/:id', connectLiveReload(), patientController.showPatientDashboard);
 router.post('/new', patientController.createPatient);
